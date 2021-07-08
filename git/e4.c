843ec4f58ad598e6f7650a92a7eb5d17159a1a85
michaeltakao <s1280051@u-aizu.ac.jp>
2021-07-08 23:31:35 +0900

    Greetings added

--- git/e4.c
+++ git/e4.c
@@ -1,17 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
-
 
 int main(){
-int n1,n1;
-printf("Rolling the dice...\n");
-srand(time(NULL));
-n1 = 1 + rand() % 6;
-n2 = 1 + rand() % 6;
+char st[256];
 
-printf("Die 1: %d\n,n1);
-printf("Die 2: %d\n,n2);
-printf("Total value: %d\n",n1 + n2);
+printf("What is your name?\n);
+scanf("%s",str);
+printf("Hello, %s!\n,st);
 return 0;
 }
